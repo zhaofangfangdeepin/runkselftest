1ea0f7eebdce42896717d0576aa0a446e50a714d
zhaofangfang <zhaofangfang@deepin.com>
2019-08-14 09:59:58 +0800

    增加内核安装和配置过程

--- runkselftest.sh
+++ runkselftest.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+if [ `whoami` = root ];then
+	echo "正在使用root用户，准备测试"
+else
+	echo "请使用root用户"
+	echo 1
+fi
+dpkg -l linux-source 
+if [[ 0 -eq $? ]] && [[ -d /usr/src/linux-source-4.19 ]];then
+	cd /usr/src/linux-source-4.19/
+else
+	echo "下载并解压linux-source"
+	apt-get install linux-source -y 
+	cd /usr/src
+	xz -d linux-source-4.19.tar.xz
+	tar -xvf linux-source-4.19.tar
+	cd linux-source-4.19
+fi
+
+echo "安装所需的包"
+apt-get install -y libpopt-dev libfuse-dev llvm linux-libc-dev pkgconf rsync libcap-dev libcap-ng-dev libnuma-dev libpython2.7-dev libpython3.7-dev > /dev/null 2>&1
+
+echo "build kernel selftest"
+cp cp /boot/config-`uname -r` .config
+yes| sudo  make oldconfig
+make -C tools/testing/selftests 
 logname=`date "+%Y-%m-%d%H:%M:%S"`
 touch kselftest$logname.log
 if [ $# -eq 0 ];then
