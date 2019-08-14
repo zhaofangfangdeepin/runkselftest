1fc09ee3d38b2026826b7b6929593a9f1b8d9007
zhaofangfang <zhaofangfang@deepin.com>
2019-08-14 10:09:25 +0800

    修改bug

--- runkselftest.sh
+++ runkselftest.sh
@@ -3,10 +3,13 @@ if [ `whoami` = root ];then
 	echo "正在使用root用户，准备测试"
 else
 	echo "请使用root用户"
-	echo 1
+	exit 1
 fi
-dpkg -l linux-source 
+
+dpkg -l linux-source > /dev/null 2>&1
+
 if [[ 0 -eq $? ]] && [[ -d /usr/src/linux-source-4.19 ]];then
+	cp kselftest /usr/src/linux-source-4.19
 	cd /usr/src/linux-source-4.19/
 else
 	echo "下载并解压linux-source"
@@ -14,6 +17,7 @@ else
 	cd /usr/src
 	xz -d linux-source-4.19.tar.xz
 	tar -xvf linux-source-4.19.tar
+	cp kselftest /usr/src/linux-source-4.19
 	cd linux-source-4.19
 fi
 
@@ -21,7 +25,7 @@ echo "安装所需的包"
 apt-get install -y libpopt-dev libfuse-dev llvm linux-libc-dev pkgconf rsync libcap-dev libcap-ng-dev libnuma-dev libpython2.7-dev libpython3.7-dev > /dev/null 2>&1
 
 echo "build kernel selftest"
-cp cp /boot/config-`uname -r` .config
+cp /boot/config-`uname -r` .config
 yes| sudo  make oldconfig
 make -C tools/testing/selftests 
 logname=`date "+%Y-%m-%d%H:%M:%S"`
