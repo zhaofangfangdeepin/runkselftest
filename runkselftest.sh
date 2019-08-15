ef270d3f4a8034e9a255435c664c9c3e145ca331
zhaofangfang <zhaofangfang@deepin.com>
2019-08-15 16:01:12 +0800

    修改脚本

--- runkselftest.sh
+++ runkselftest.sh
@@ -6,18 +6,19 @@ else
 	exit 1
 fi
 
+tests=(bpf capabilities cgroup cpufreq cpu-hotplug efivarfs exec filesystems firmware ftrace futex gpio intel_pstate ipc kcmp kvm lib membarrier memfd memory-hotplug mount mqueue net netfilter nsfs powerpc proc pstore ptrace rseq rtc seccomp sigaltstack size sparc64 splice static_keys sync sysctl timers user vm x86 zram)
+
 dpkg -l linux-source > /dev/null 2>&1
 
 if [[ 0 -eq $? ]] && [[ -d /usr/src/linux-source-4.19 ]];then
-	cp kselftest /usr/src/linux-source-4.19
 	cd /usr/src/linux-source-4.19/
+	make clean
 else
 	echo "下载并解压linux-source"
 	apt-get install linux-source -y 
 	cd /usr/src
 	xz -d linux-source-4.19.tar.xz
 	tar -xvf linux-source-4.19.tar
-	cp kselftest /usr/src/linux-source-4.19
 	cd linux-source-4.19
 fi
 
@@ -31,15 +32,14 @@ make -C tools/testing/selftests
 logname=`date "+%Y-%m-%d%H:%M:%S"`
 touch kselftest$logname.log
 if [ $# -eq 0 ];then
-	while read line
-	do
-			make TARGETS=$line O=/home/deepin/kselftest/$line summary=1 kselftest > /dev/null 2>&1
+	for test in "${tests[@]}";do
+			make TARGETS=$test O=/home/deepin/kselftest/$test summary=1 kselftest > /dev/null 2>&1
 			if [ $? -eq 0 ];then
-	   			echo "Kselftest $line  test succeed!" >>kselftest$logname.log
+	   			echo "Kselftest $test  test succeed!" >>kselftest$logname.log
    			else
-	   			echo "Kselftest $line  test failed!"  >>kselftest$logname.log
+	   			echo "Kselftest $test  test failed!"  >>kselftest$logname.log
     			fi
-	done < kselftest
+	done
 else
 	for arg in "$*"
 	do
